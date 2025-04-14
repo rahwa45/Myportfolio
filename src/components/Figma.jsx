d3f5b627a15b80f41f768e50fd9c070c77c232b0
Rahwa Gebreyesus <rahwa3113@gmail.com>
2025-04-14 22:52:01 +0300

    Redeploying

--- src/components/Figma.jsx
+++ src/components/Figma.jsx
@@ -1,30 +1,75 @@
 import React from "react";
 
 const Figma = () => {
-  const images = [
-    "fig1.png",
-    "fig2.png",
-    "fig3.png",
-    "fig4.png",
-    "fig5.png",
-    "fig7.png",
-    "fig6.png",
-  ];
-
   return (
     <div className="container py-5">
       <h2 className="text-center fw-bold mb-4">My Figma Designs</h2>
 
       <div className="row g-5">
-        {images.map((src, index) => (
-          <div className="col-md-6 col-lg-4" key={index}>
-            <img
-              src={`./${src}`}
-              alt={`Figma ${index + 1}`}
-              className="img-fluid rounded shadow-lg"
-            />
-          </div>
-        ))}
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig1.png" // Path to your image
+            alt="Figma 1"
+            className="img-fluid rounded shadow-lg"
+            width={500} // Optional, to define the width
+            height={300} // Optional, to define the height
+          />
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig2.png" // Path to your image
+            alt="Figma 2"
+            className="img-fluid rounded shadow-lg"
+            width={500}
+            height={300}
+          />
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig3.png" // Path to your image
+            alt="Figma 3"
+            className="img-fluid rounded shadow-lg"
+            width={500}
+            height={300}
+          />
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig4.png" // Path to your image
+            alt="Figma 4"
+            className="img-fluid rounded shadow-lg"
+            width={500}
+            height={300}
+          />
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig5.png" // Path to your image
+            alt="Figma 5"
+            className="img-fluid rounded shadow-lg"
+            width={500}
+            height={300}
+          />
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig7.png" // Path to your image
+            alt="Figma 7"
+            className="img-fluid rounded shadow-lg"
+            width={500}
+            height={300}
+          />
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <img
+            src="./fig6.png" // Path to your image
+            alt="Figma 6"
+            className="img-fluid rounded shadow-lg"
+            width={500}
+            height={300}
+          />
+        </div>
       </div>
     </div>
   );
